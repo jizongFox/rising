cb7ac6efdd9ef30467caac8b9d62cc0ac65c388f
jizong <jizong.peng.1@etsmtl.net>
2021-10-23 21:54:23 -0400

    resolved partially imported errors by extracting ntuples to utils folder.

--- rising/transforms/abstract.py
+++ rising/transforms/abstract.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, Sequence, Tuple, Union, TypeVar
 import torch
 
 from rising.random import AbstractParameter, DiscreteParameter
-from rising.transforms.format import ntuple
+from rising.utils.mise import ntuple
 
 __all__ = ["AbstractTransform", "BaseTransform", "PerSampleTransform", "PerChannelTransform", "BaseTransformSeeded"]
 
---
+++ rising/utils/mise.py
@@ -0,0 +1,29 @@
+import collections
+from itertools import repeat
+
+import numpy as np
+from torch import Tensor
+
+
+def ntuple(n):
+    def parse(x):
+        if isinstance(x, (Tensor, np.ndarray, str)):
+            return tuple(repeat(x, n))
+        if isinstance(x, collections.abc.Iterable):
+            item_list = list(x)
+            if len(item_list) == n:
+                return item_list
+            if len(item_list) == 1:
+                return tuple(repeat(item_list[0], n))
+            if len(list(x)) != n:
+                raise RuntimeError(f"Iterable shape inconsistent, n = {n}, given {len(list(x))}")
+
+        return tuple(repeat(x, n))
+
+    return parse
+
+
+single = ntuple(1)
+pair = ntuple(2)
+triple = ntuple(3)
+quadruple = ntuple(4)
